aecbc23ef98f3180dfbfcf61e94079cb464a522e
kwall <kwall@apache.org>
2021-12-09 14:46:27 +0000

    Revert "Disable shellcheck SC2295 to avoid a behavioural difference validating scripts with shellcheck 0.8.0"
    This reverts commit 789b2db97fbecff427982140f462666ad0955b85.

--- cicd/pr_check.sh
+++ cicd/pr_check.sh
@@ -2,6 +2,5 @@
 
 docker pull quay.io/app-sre/mk-ci-tools:latest
 docker run -u $(id -u) -v $(pwd):/opt/strimzi-operator:z \
-    -e SHELLCHECK_OPTS='--exclude=SC2295' \
     quay.io/app-sre/mk-ci-tools:latest \
     "bash" "-c" "cd /opt/strimzi-operator && make 'SUBDIRS=kafka-agent mirror-maker-agent tracing-agent crd-annotations test crd-generator api mockkube certificate-manager operator-common config-model config-model-generator cluster-operator topic-operator user-operator kafka-init packaging/helm-charts/helm3 packaging/install packaging/examples' MVN_ARGS='-DskipITs -DskipTests=true -Dgroups=oauth' all"
