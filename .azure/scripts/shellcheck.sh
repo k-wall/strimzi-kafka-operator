06ed1a4072208df334afb722718bf48a93161728
kwall <kwall@apache.org>
2021-12-09 14:47:38 +0000

    Revert "debug - log shellcheck version"
    This reverts commit f374ffa734a349ba9f3076004cd21cb89325465f.

--- .azure/scripts/shellcheck.sh
+++ .azure/scripts/shellcheck.sh
@@ -13,8 +13,6 @@ docker-images/kafka/exporter-scripts/*.sh
 docker-images/jmxtrans/*.sh
 tools/olm-bundle/*.sh"
 
-shellcheck --version
-
 for SCRIPTS in $SCRIPT_DIRS; do
     shellcheck -a -P $(dirname "$SCRIPTS") -x "$SCRIPTS"
 done
