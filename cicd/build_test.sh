0763a362d1148bf5c8a307d0850ba09a89d84ff8
Stanislav Kaleta <skaleta@redhat.com>
2022-02-08 15:43:21 +0000

    Update build_test.sh
    (cherry picked from commit 8fd3efd05651a83a5d97f5b95b7d5078de6b57de)

--- cicd/build_test.sh
+++ cicd/build_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export ST_KAFKA_VERSION=2.8.1
+export ST_KAFKA_VERSION=2.7.0
 export COMPONENTS_IMAGE_PULL_POLICY=Always
 export TEST_CLIENT_IMAGE=quay.io/strimzi/test-client:0.26.1-kafka-2.8.1
 export TEST_PRODUCER_IMAGE=quay.io/mk-ci-cd/java-kafka-producer:latest
