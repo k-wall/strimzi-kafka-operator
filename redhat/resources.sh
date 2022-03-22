9754a60c3f1fc745c5d127699c492e1c89037b78
Michael Edgar <medgar@redhat.com>
2022-03-22 12:00:54 -0400

    [MGDSTRM-7145] Remove `make` use from resources packaging script
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- redhat/resources.sh
+++ redhat/resources.sh
@@ -6,6 +6,5 @@ export STRIMZI_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.1:ev
 echo "Deploying resources ${STRIMZI_VERSION}"
 echo ""
 
-make -C packaging/install release RELEASE_VERSION=${STRIMZI_VERSION}
-tar -z -cf strimzi-resources-${STRIMZI_VERSION}.tar.gz -C ./strimzi-${STRIMZI_VERSION}/install/ .  || [[ $? -eq 1 ]]
+tar -z -cf strimzi-resources-${STRIMZI_VERSION}.tar.gz -C ./packaging/install/ .  || [[ $? -eq 1 ]]
 mvn deploy:deploy-file -Durl=${AProxDeployUrl} -DrepositoryId=indy-mvn -Dfile=strimzi-resources-${STRIMZI_VERSION}.tar.gz -Dpackaging=tar.gz -DgroupId=io.strimzi -DartifactId=strimzi-resources -Dversion=${STRIMZI_VERSION}
